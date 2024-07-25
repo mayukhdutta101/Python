13f67bb21b051e1716763323292620fbb219889b
Mayukh_Dutta_MCA_2A <125433536+mayukhdutta101@users.noreply.github.com>
2024-07-25 20:37:35 +0530

    Question_2.py

--- Assignment 2/Question_2.py
+++ Assignment 2/Question_2.py
@@ -1,6 +1,6 @@
 # To solve the quadratic equation.
 
-import math
+import cmath
 
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
@@ -8,7 +8,7 @@ c = int(input("Enter c: "))
 
 d = (b**2) - (4*a*c)
 
-sol1 = (-b-math.sqrt(d))/(2*a)
-sol2 = (-b+math.sqrt(d))/(2*a)
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
 
 print('The solution are {0} and {1}'.format(sol1,sol2))
